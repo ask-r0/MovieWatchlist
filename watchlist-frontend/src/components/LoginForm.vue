e6ac6d626ba999e076409f6bd2903420bea5648d
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2022-04-09 17:16:49 +0200

    changes

--- watchlist-frontend/src/components/LoginForm.vue
+++ watchlist-frontend/src/components/LoginForm.vue
@@ -1,13 +1,69 @@
 <template>
+  <Card style="width: 25rem; margin-bottom: 2em">
+    <template #title class="col-offset-2">
+      Welcome!
+    </template>
+    <template #content>
+      <span class="p-float-label">
+            <InputText id="username" type="text" v-model="watchlistCode" />
+            <label for="username">Watchlist Code</label>
+      </span>
+    </template>
+    <template #footer>
+      <Button label="Enter"></Button>
+    </template>
+  </Card>
+  <Card style="width: 25rem; margin-bottom: 2em">
+    <template #content>
+      <span class="p-float-label">
+            <InputText id="username" type="text" v-model="watchlistCode" />
+            <label for="username">Watchlist Code</label>
+      </span>
+      <span class="p-float-label">
+            <InputText id="username" type="text" v-model="watchlistCode" />
+            <label for="username">Watchlist Code</label>
+      </span>
+      <span class="p-float-label">
+            <InputText id="username" type="text" v-model="watchlistCode" />
+            <label for="username">Watchlist Code</label>
+      </span>
 
+    </template>
+  </Card>
+  <p>{{watchlistCode}}</p>
 </template>
 
 <script>
+import Card from 'primevue/card';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
 export default {
-  name: "LoginForm"
+  name: "LoginForm",
+  components: { Card, InputText, Button },
+  data() {
+    return {
+      watchlistCode: ''
+    }
+  }
+
 }
 </script>
 
 <style scoped>
+
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+#footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
 </style>
--- watchlist-frontend/src/main.js
+++ watchlist-frontend/src/main.js
@@ -8,9 +8,9 @@ import "primevue/resources/themes/saga-blue/theme.css";       //theme
 import "primevue/resources/primevue.min.css";                 //core css
 import "primeicons/primeicons.css";                           //icons
 
-import PrimeVue from 'primevue/config';
-import Button from "primevue/button";
+import 'primeflex/primeflex.css';
 
+import PrimeVue from 'primevue/config';
 
 const app = createApp(App);
 
