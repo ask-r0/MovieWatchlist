0667d196aa75fa9c10473913358855ae3f2db43e
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2022-04-17 18:24:49 +0200

    Added RestTemplate bean

--- watchlist-backend/src/main/java/no/home/moviewatchlist/MovieWatchlistApplication.java
+++ watchlist-backend/src/main/java/no/home/moviewatchlist/MovieWatchlistApplication.java
@@ -2,12 +2,19 @@ package no.home.moviewatchlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieWatchlistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieWatchlistApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
