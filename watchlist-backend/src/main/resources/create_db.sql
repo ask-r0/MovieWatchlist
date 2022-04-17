14efe7938eb447c8e7dcfd301c74383cbaead563
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2022-04-17 18:22:15 +0200

    Updated db script

--- watchlist-backend/src/main/resources/create_db.sql
+++ watchlist-backend/src/main/resources/create_db.sql
@@ -5,11 +5,8 @@ CREATE TABLE watchlist (
     PRIMARY KEY (watchlist_code)
 );
 
-CREATE TABLE movie (
+CREATE TABLE movie_watchlist (
+    watchlist_code VARCHAR(64) NOT NULL,
     movie_id INT NOT NULL,
-    PRIMARY KEY (movie_id)
+    PRIMARY KEY (watchlist_code, movie_id)
 );
-
-INSERT INTO watchlist(watchlist_code, watchlist_name, password) VALUES
-("nicestwatchlist", "my watchlist", "$2a$12$XFfTaj9FMomnlfkD1DkMXuJvgaNcqUIQKRDmOv2qd/Flci5ZH96ZO");
-/* unencrypted password: "" */
