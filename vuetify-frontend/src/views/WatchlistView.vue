56ec21580027ef02f7ecc85578cdd7fda4fb71d3
ask-r0 <75451424+ask-r0@users.noreply.github.com>
2022-04-18 13:05:39 +0200

    Switched to using composition API

--- vuetify-frontend/src/views/WatchlistView.vue
+++ vuetify-frontend/src/views/WatchlistView.vue
@@ -14,7 +14,7 @@
         mdi-minus-circle
       </v-icon>
     </v-btn>
-    <v-btn variant="outlined" class="ml-10" loading>
+    <v-btn variant="outlined" class="ml-10" @click="onExit">
       Exit
       <v-icon right>
         mdi-exit-to-app
@@ -29,78 +29,93 @@
 </template>
 
 <script>
-import MovieCard from "../components/MovieCard.vue";
+import { defineComponent } from "vue";
 import MovieList from "../components/MovieList.vue";
-export default {
-  name: "WatchlistView",
-  components: { MovieList, MovieCard },
-  data() {
-    return {
-      movies: [
-        {
-          movieId: 567,
-          movieName: "Interstellar",
-          movieYear: "2002",
-          movieRating: "8.9",
-          movieDirector: "Christopher Nolan",
-          movieTime: "130min",
-          movieImage:
+import { useRouter, useRoute } from "vue-router";
+import { useWatchlistStore } from "../stores/watchlist.js";
+
+export default defineComponent({
+  components: { MovieList },
+  setup() {
+    const watchlistStore = useWatchlistStore();
+    const router = useRouter();
+
+    function onExit() {
+      watchlistStore.$reset();
+      router.push({name:"home"});
+    }
+
+
+    let movies = [
+      {
+        movieId: 567,
+        movieName: "Interstellar",
+        movieYear: "2002",
+        movieRating: "8.9",
+        movieDirector: "Christopher Nolan",
+        movieTime: "130min",
+        movieImage:
             "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-        },
-        {
-          movieId: 244,
-          movieName: "Pulp Fiction",
-          movieYear: "1989",
-          movieRating: "9.1",
-          movieDirector: "Quentin Tarantino",
-          movieTime: "114min",
-          movieImage:
+      },
+      {
+        movieId: 244,
+        movieName: "Pulp Fiction",
+        movieYear: "1989",
+        movieRating: "9.1",
+        movieDirector: "Quentin Tarantino",
+        movieTime: "114min",
+        movieImage:
             "https://image.tmdb.org/t/p/original/fIE3lAGcZDV1G6XM5KmuWnNsPp1.jpg",
-        },
-        {
-          movieId: 899,
-          movieName: "Parasite",
-          movieYear: "2020",
-          movieRating: "8.6",
-          movieDirector: "Bong Joon Ho",
-          movieTime: "148min",
-          movieImage:
+      },
+      {
+        movieId: 899,
+        movieName: "Parasite",
+        movieYear: "2020",
+        movieRating: "8.6",
+        movieDirector: "Bong Joon Ho",
+        movieTime: "148min",
+        movieImage:
             "https://image.tmdb.org/t/p/original/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
-        },
-        {
-          movieId: 567,
-          movieName: "Interstellar",
-          movieYear: "2002",
-          movieRating: "8.9",
-          movieDirector: "Christopher Nolan",
-          movieTime: "130min",
-          movieImage:
-              "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-        },
-        {
-          movieId: 244,
-          movieName: "Pulp Fiction",
-          movieYear: "1989",
-          movieRating: "9.1",
-          movieDirector: "Quentin Tarantino",
-          movieTime: "114min",
-          movieImage:
-              "https://image.tmdb.org/t/p/original/fIE3lAGcZDV1G6XM5KmuWnNsPp1.jpg",
-        },
-        {
-          movieId: 899,
-          movieName: "Parasite",
-          movieYear: "2020",
-          movieRating: "8.6",
-          movieDirector: "Bong Joon Ho",
-          movieTime: "148min",
-          movieImage:
-              "https://image.tmdb.org/t/p/original/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
-        },
-      ],
-    };
+      },
+      {
+        movieId: 567,
+        movieName: "Interstellar",
+        movieYear: "2002",
+        movieRating: "8.9",
+        movieDirector: "Christopher Nolan",
+        movieTime: "130min",
+        movieImage:
+            "https://image.tmdb.org/t/p/original/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+      },
+      {
+        movieId: 244,
+        movieName: "Pulp Fiction",
+        movieYear: "1989",
+        movieRating: "9.1",
+        movieDirector: "Quentin Tarantino",
+        movieTime: "114min",
+        movieImage:
+            "https://image.tmdb.org/t/p/original/fIE3lAGcZDV1G6XM5KmuWnNsPp1.jpg",
+      },
+      {
+        movieId: 899,
+        movieName: "Parasite",
+        movieYear: "2020",
+        movieRating: "8.6",
+        movieDirector: "Bong Joon Ho",
+        movieTime: "148min",
+        movieImage:
+            "https://image.tmdb.org/t/p/original/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
+      },
+    ];
+
+    return {
+      movies,
+      onExit
+    }
   },
-};
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
